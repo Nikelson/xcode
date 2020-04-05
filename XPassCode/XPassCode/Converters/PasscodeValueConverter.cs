8c965b3654b3a95ce4fb6d0300533d4515e0469b
nickq <nick.q@live.com>
2020-04-05 14:11:06 +0400

    init

---
+++ XPassCode/XPassCode/Converters/PasscodeValueConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XPassCode.Converters
+{
+
+    public class PasscodeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string val = (string)value;
+            string param = (string)parameter;
+
+            int index = string.IsNullOrEmpty(param) ? 0 : int.Parse(param);
+
+            if (string.IsNullOrEmpty(val) || val.Length <= index) {
+                return "";
+            }
+
+            return val[index];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new Exception();
+        }
+    }
+}
---
+++ XPassCode/XPassCode/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace XPassCode
+{
+
+    //public class Data
+    //{
+    //    public string Username { get; set; }
+    //    public string Password { get; set; }
+    //    public string AuthMethod { get; set; }
+    //    public string DeviceId { get; set; }
+    //    public string IP { get; set; }
+
+    //    public Data()
+    //    {
+    //        AuthMethod = "FaceId";
+    //        DeviceId = "1";
+    //        IP = "172.0.0.1";
+    //    }
+
+    //}
+    public class Data
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string AuthMethod { get; set; }
+        public string DeviceId { get; set; }
+        public string IP { get; set; }
+        public string PushNotificationId { get; set; }
+        public bool Disabled { get; set; }
+
+        public Data()
+        {
+            AuthMethod = "UP";
+            DeviceId = "beeef4d393ec2c98";
+            IP = null;
+            Disabled = false;
+            PushNotificationId = "";
+        }
+
+    }
+
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            PasscodeEntry.Focus();
+        }
+
+        private void PasscodeEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Entry entry = sender as Entry;
+            string val = entry.Text;
+
+            if (val.Length > 4)
+            {
+                val = val.Remove(val.Length - 1);
+                entry.Text = val;
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+            Data data = new Data
+            {
+                Username = username.Text,
+                Password = password.Text
+            };
+            resultText.Text = "Loading...";
+
+            await MakePost(data);
+        }
+
+        public async Task MakePost(Data data)
+        {
+            using (var client = new HttpClient())
+            {
+                var uri = new Uri("https://test-videobanking.vtb.ge/api/sessions/authenticate/mobileClient");
+
+                var json = JsonConvert.SerializeObject(data);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage result;
+
+                try
+                {
+                    result = await client.PostAsync(uri, content);
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+
+                var resultString = await result.Content.ReadAsStringAsync();
+                resultText.Text = resultString;
+
+                Console.WriteLine(resultString);
+            }
+        }
+    }
+}
